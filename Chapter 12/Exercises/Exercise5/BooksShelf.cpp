7582a5fbe822263559a84d9c45c243ccd203b00b
Alejandro Ríos <76874671+alxrios@users.noreply.github.com>
2023-04-26 17:20:58 +0200

    feat: updated

--- Chapter 12/Exercises/Exercise5/BooksShelf.cpp
+++ Chapter 12/Exercises/Exercise5/BooksShelf.cpp
@@ -25,7 +25,6 @@ void BooksShelf::addBook() {
 		std::cin >> autnum;
 	}
 	book.setAuthors(autnum);
-	book.showAuthors();
 	books[booksInShelf] = book;
 	booksInShelf++;
 	printSize();
@@ -53,14 +52,73 @@ int BooksShelf::searchBook(std::string bookTitle) {
 	return(pos);
 }
 
-void BooksShelf::showOneBook(std::string bookTitle, int option) { // Complete this function
-	int pos = searchBook(bookTitle);
-	if ((pos >= 0) & (pos < 100)) {
-		books[pos].showTitle();
-		books[pos].showISBN();
-		books[pos].showAuthors();
-		books[pos].showPublisher();
-		books[pos].showPrice();
-		books[pos].showNumOfCopies();
+void BooksShelf::showOneBook(int pos) {
+	books[pos].showTitle();
+	books[pos].showISBN();
+	books[pos].showAuthors();
+	books[pos].showPublisher();
+	books[pos].showPrice();
+	books[pos].showNumOfCopies();
+}
+
+int BooksShelf::searchByISBN(std::string strISBN) {
+	while (!checkStrISBN(strISBN)) {
+		std::cout << "\nISBN introduced is incorrect, please introduce a valid one: ";
+		getline(std::cin, strISBN);
 	}
+	int pos = -1;
+	for (int i = 0; i < booksInShelf; i++) {
+		if (strISBN == books[i].getStrISBN()) {
+			pos = i;
+			break;
+		}
+	}
+	return(pos);
+}
+
+bool BooksShelf::checkStrISBN(std::string strISBN) {
+	// First step, convert the string input to int array format
+	int i;
+	int numericISBN[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	for (i = 0; i < 9; i++) {
+		numericISBN[i] = static_cast<int>(strISBN[i]) - 48;
+	}
+	if (strISBN[9] == 'X') {
+		numericISBN[9] = 10;
+	} else {
+		numericISBN[9] = static_cast<int>(strISBN[9]) - 48;
+	}
+	
+	// Apply same algorithm as in checkISBN function of class BookType
+	bool result;
+	int digit = -1;
+	int sum = 0;
+	
+	for (i = 1; i <= 9; i++) {
+		sum += (11 - i)*numericISBN[i - 1];
+	}
+	
+	int correctLast = -1;
+	for (i = 0; i < 11; i++) {
+		if (((sum + i) % 11 ) == 0) {
+			correctLast = i;
+			break;
+		}
+	}
+	
+	if (correctLast == numericISBN[9]) {
+		result = 1;
+	} else {
+		result = 0;
+	}
+	
+	return(result);	
+}
+
+int BooksShelf::getBooksInShelf() {
+	return(booksInShelf);
+}
+
+void BooksShelf::setBooksInShelf(int booksQuantity) {
+	booksInShelf = booksQuantity;
 }
