99cccb466351d8377a1c2e0efdea500d2c40187c
Alejandro Ríos <76874671+alxrios@users.noreply.github.com>
2023-04-26 17:22:07 +0200

    feat: updated

--- Chapter 12/Exercises/Exercise5/BooksShelf.h
+++ Chapter 12/Exercises/Exercise5/BooksShelf.h
@@ -2,8 +2,6 @@
 
 class BooksShelf {
 	public:
-		// int booksInShelf = 0;
-		// Current number of books in shelf (max 100)
 		BooksShelf(int);
 		void printSize();
 			// Prints the current number of books in the shelf
@@ -15,20 +13,25 @@ class BooksShelf {
 			// Add a single book to the shelf
 		void showBooks();
 			// Prints all books information
-		void showOneBook(std::string, int);
-			// Shows the information of the book with the title
-			// passed as parameter or the ISBN in string format
-			// Option is choose in the second parameter which must be 1 or 2
+		void showOneBook(int);
+			// Shows the information of the book with position passed
+			// as parameter
 		int searchBook(std::string);
 			// Search a book by it's title and returns its position
 			// in the book's array
 		int searchByISBN(std::string);
 			// Search a book by its ISBN in string format
-			// It returns its position in the books array
-	private:
+		int getBooksInShelf();
+			// Returns the number of books in shelf a the moment
+		void setBooksInShelf(int);
+			// Sets the quantity of books in shelf to the number
+			// given as parameter
 		BookType books[100];
+			
+	private:
 		int booksInShelf;
-		// Current number of books in shelf (max 100)
+			// Current number of books in shelf (max 100)
+		bool checkStrISBN(std::string);
+			// Checks the string version of ISBN
+			// To be used the searchByISBN function
 };
-
-
